8891f89251d1dae2b7101690c813b4e44bda8533
Alex Storrs <astorrs27@gmail.com>
2024-12-08 15:16:52 -0500

    Day 3 complete

--- 2015/Day 3 - Perfectly Spherical Houses in a Vacuum.py
+++ 2015/Day 3 - Perfectly Spherical Houses in a Vacuum.py
@@ -1,3 +1,4 @@
+'''
 # Part 1
 
 directions = ""
@@ -13,6 +14,32 @@ moves = {"v":(0, -1), "^":(0, 1), "<":(-1, 0), ">":(1, 0)}
 for char in directions:
     pos[0] += moves[char][0]
     pos[1] += moves[char][1]
-    visited.add(str(pos[0]) + str(pos[1]))
+    visited.add(str(pos[0]) + ":" + str(pos[1]))
+
+print(len(visited))
+'''
+
+# Part 2
+
+directions = ""
+
+with open('2015/data/day3.txt') as file:
+    for line in file:
+        directions += line.strip()
+
+pos1 = [0, 0]
+pos2 = [0, 0]
+visited = {str(pos1[0]) + ":" + str(pos1[1])}
+moves = {"v":(0, -1), "^":(0, 1), "<":(-1, 0), ">":(1, 0)}
+
+for i, char in enumerate(directions):
+    if i % 2 == 0:
+        pos1[0] += moves[char][0]
+        pos1[1] += moves[char][1]
+        visited.add(str(pos1[0]) + ":" + str(pos1[1]))
+    else:
+        pos2[0] += moves[char][0]
+        pos2[1] += moves[char][1]
+        visited.add(str(pos2[0]) + ":" + str(pos2[1]))
 
 print(len(visited))
