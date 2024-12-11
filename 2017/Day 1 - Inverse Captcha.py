59d54382bb75d3adcebb3a85f0a6b95284264af1
Alex Storrs <astorrs27@gmail.com>
2024-12-10 21:40:11 -0500

    Day 1 complete (formatting)

--- 2017/Day 1 - Inverse Captcha.py
+++ 2017/Day 1 - Inverse Captcha.py
@@ -1,4 +1,23 @@
+'''
 # Part 1
+
+nums = []
+
+with open('2017/data/day1.txt') as file:
+    for line in file:
+        nums += list(line.strip())
+
+nums = [int(x) for x in nums] + [int(nums[0])]
+sum = 0
+
+for i in range(len(nums) - 1):
+    if nums[i] == nums[i + 1]:
+        sum += nums[i]
+
+print(sum)
+'''
+
+# Part 2
 
 nums = []
 
