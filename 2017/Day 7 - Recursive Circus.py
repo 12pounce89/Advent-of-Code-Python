2eb85c13cff462bafdc1162fcaf73b0c2334de0d
12pounce89 <astorrs27@gmail.com>
2024-12-12 16:03:48 -0500

    Day 7 part 1 done, part 2 progress

---
+++ 2017/Day 7 - Recursive Circus.py
@@ -0,0 +1,83 @@
+'''
+# Part 1
+
+allPrograms = set()
+above = set()
+
+with open('2017/data/day7.txt') as file:
+    for line in file:
+        broken = line.strip().split(" -> ")
+        if len(broken) > 1:
+            upper = broken[1].split(", ")
+            for item in upper:
+                above.add(item)
+        programs.add(broken[0].split(" ")[0])
+
+for item in programs:
+    if item not in above:
+        print(item)
+        break
+'''
+
+# Part 2
+
+programs = {}
+weights = {}
+allPrograms = set()
+above = set()
+fixed = 0
+
+def get_weight(program):
+    global fixed
+    # print('test')
+    # print(fixed, 'found')
+    totalWeight = weights[program]
+    if program in programs:
+        towers = {}
+        for item in programs[program]:
+            aboveWeight, aboveTower = get_weight(item)
+            if aboveWeight not in towers:
+                # if len(towers) == 0:
+                    # print('test')
+                towers[aboveWeight] = 0
+                # else:
+                #     problem.append(aboveTower)
+                #     for key in towers:
+                #         problem.append(key)
+                #         aboveWeight = key
+            towers[aboveWeight] += 1
+        if len(towers) > 1:
+            print(program, towers)
+            for key in towers:
+                if towers[key] > 1:
+                    totalWeight += key * towers[key]
+                    proper = key
+                if towers[key] == 1:
+                    wrong = key
+            fixed = weights[program] - (wrong - proper)
+        totalWeight += aboveWeight * towers[aboveWeight]
+    return totalWeight, program
+
+with open('2017/data/tester.txt') as file:
+    for line in file:
+        broken = line.strip().split(") -> ")
+        weights[broken[0].split()[0]] = int(broken[0].split(" (")[1].split(")")[0])
+        if len(broken) > 1:
+            base = broken[0].split()[0]
+            programs[base] = []
+            upper = broken[1].split(", ")
+            for item in upper:
+                programs[base].append(item)
+                above.add(item)
+        allPrograms.add(broken[0].split(" ")[0])
+
+for item in programs:
+    if item not in above:
+        base = item
+        break
+
+get_weight(base)
+
+print(fixed)
+# print(problem)
+# print(weights[problem[0]] - problem[1])
